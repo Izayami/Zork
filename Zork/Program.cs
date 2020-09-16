78f92f7f7a8edf5bccd4132ba2aaabb3a2bc5a8b
Izayami <52263008+Izayami@users.noreply.github.com>
2020-09-15 23:24:51 -0400

    Game Loop 1.3
    Game Loop 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,9 +8,46 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                
+                    case Commands.LOOK:
+                        outputString = "A rubber mat saying 'Welcome to Zork!.' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                        outputString = "You moved NORTH.";
+                        break;
+
+                    case Commands.SOUTH:
+                        outputString = "You moved South.";
+                        break;
+
+                    case Commands.WEST:
+                        outputString = "You moved West.";
+                        break;
+
+                    case Commands.EAST:
+                        outputString = "You moved East.";
+                        break;
+
+                    default:
+                        outputString = "Unknown Command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
