3e8347c35fa04771ff1ba4bcdf8eab330b29da30
Izayami <52263008+Izayami@users.noreply.github.com>
2020-10-01 21:15:30 -0400

    Zork 3.2
    Zork 3.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -19,12 +19,14 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
             InitializeRoomDescriptions();
+
+            Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
                 Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
+                command = ToCommand(Console.ReadLine().Trim());              
 
                 switch (command)
                 {
@@ -44,11 +46,16 @@ namespace Zork
                         {
                             Console.WriteLine("The way is shut!");
                         }
-                        break;
+                        break;                   
 
                     default:
                         Console.WriteLine("Unknown Command.");
                         break;
+                }
+                if (previousRoom != CurrentRoom)
+                {
+                    Console.WriteLine(CurrentRoom.Description);
+                    previousRoom = CurrentRoom;
                 }
             }
         }
@@ -98,19 +105,25 @@ namespace Zork
         };
         private static void InitializeRoomDescriptions()
         {
-            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
-            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+            var roomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+            
+            {
+                roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
+                roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+                roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+                roomMap["Forest"].Description = "This is a forest, with  tress in all directions around you.";
+                roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
+                roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
 
-            Rooms[1, 0].Description = "This is a forest, with  tress in all directions around you.";
-            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
-            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-
-            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+                roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+                roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+                roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+            }
         }
         private static (int Row, int Column) Location = (1, 1);
     }
 }
-    
