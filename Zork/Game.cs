968cd1238242110d35841636bc1bcfd34addb8b0
Izayami <52263008+Izayami@users.noreply.github.com>
2020-10-23 23:54:15 -0400

    4.3 Zork
    4.3 Zork

--- Zork/Game.cs
+++ Zork/Game.cs
@@ -1,17 +1,65 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
     public class Game
     {
-        public World World { get; }
-    }
-    public static Game Load(string filename)
-    {
-        Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
-        return game;
+        public World World { get; private set; }
+
+        [JsonIgnore]
+        public Player Player { get, private set; }
+
+        public Game(World world, Player player)
+        {
+            World = world;
+            Player = player;
+        }
+
+        public void Run()
+        {
+            IsRunning = true;
+            Room previousRoom = null;
+            while (IsRunning)
+            {
+                Console.WriteLine(Player.Location);
+                if (previousRoom != Player.Location)
+                {
+                    Console.WriteLine(Player.Location.Description);
+                    previousRoom = Player.Location;
+                }
+
+                Console.WriteLine("\n> ");
+                Commands command = ToCommand(Console.ReadLine().Trim());
+
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        IsRunning - false;
+                        break;
+
+                    case Commands.LOOK:
+                        Console.WriteLine(Player.Location.Description);
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        Directions direction = Enum.Parse<Directions>(command.ToString(), true);
+                        if (Player.Move(direction) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Unkown command.");
+                        break;
+                }
+            }
+        }
     }
 }
+
